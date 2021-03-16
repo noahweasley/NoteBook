59ed3e36dfc49a44249a707c4220819b65aaa084
Noah <iebenmelu@gmail.com>
2021-03-16 07:14:14 -0700

    still building list item

--- app/UI/preload.js
+++ app/UI/preload.js
@@ -2,10 +2,13 @@ const { ipcRenderer } = require('electron')
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
+    const footer = document.querySelector('footer');
+    const footerTitle = document.querySelector('h1');
+
+    // set the footer color by system's accent color
     ipcRenderer.on('color', (_event, accentColor, isDarkMode) => {
-        const footer = document.querySelector('footer');
-        const footerTitle = document.querySelector('h1');
         footerTitle.style.color = isDarkMode ? "white" : "black";
         footer.style.backgroundColor = isDarkMode ? `#${accentColor}` : "white";
     });
+
 })
