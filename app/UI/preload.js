efef674ea28ef8d322b8cba5c6f51935cd97cdc6
Noah <iebenmelu@gmail.com>
2021-03-27 16:04:14 -0700

    Still fixing bug in tab items

--- app/UI/preload.js
+++ app/UI/preload.js
@@ -37,36 +37,43 @@ function checkToolbarItemClick() {
 
 // start sidebar check
 function checkSidebarItemClick() {
-    document.querySelector('.list-group').addEventListener('click', () => {
-        let tabItem = document.createElement('div');
-        let span = document.createElement('span');
-        let tabItemText = document.createTextNode("Hello");;
-        tabItem.classList.add('tab-item');
-        span.classList.add("icon", "icon-cancel", "icon-close-tab");
-        tabItem.appendChild(span);
-        tabItem.appendChild(tabItemText);
-        appendTabItem(tabItem);
+    document.querySelectorAll('.list-group-item')
+        .forEach(listItem => {
+            listItem.addEventListener('click', function () {
+                let tabItem = document.createElement('div');
+                let span = document.createElement('span');
+                let tabItemText = document.createElement('p');
+                tabItemText.innerText = listItem.lastElementChild.firstElementChild.innerText;
+                tabItemText.classList.add("tab-item-text");
+                tabItem.classList.add('tab-item');
+                span.classList.add("icon", "icon-cancel", "icon-close-tab");
+                tabItem.appendChild(span);
+                tabItem.appendChild(tabItemText);
+                appendTabItem(tabItem);
 
-        // append tab item 
-        function appendTabItem(element) {
-            getTabGroup().appendChild(element);
-            // Get the tab group element to append the new tab
-            function getTabGroup() {
-                // Existing or non-existing tab container
-                let tabGroup = document.querySelector('.tab-group');
-                return tabGroup ? tabGroup : createTabGroup();
-                // Create new tab group if not exists
-                function createTabGroup() {
-                    let pane = document.querySelector('.pane');
-                    if (pane) {
-                        let tabGroup = document.createElement('div');
-                        tabGroup.classList.add('tab-group');
-                        pane.appendChild(tabGroup);
+                // append tab item 
+                function appendTabItem(element) {
+                    getTabGroup().appendChild(element);
+
+                    // Get the tab group element to append the new tab
+                    function getTabGroup() {
+                        // Existing or non-existing tab container
+                        let tabGroup = document.querySelector('.tab-group');
+                        return tabGroup ? tabGroup : createTabGroup();
+
+                        // Create new tab group if not exists
+                        function createTabGroup() {
+                            let pane = document.querySelector('.pane');
+                            if (pane) {
+                                let tabGroup = document.createElement('div');
+                                tabGroup.classList.add('tab-group');
+                                pane.appendChild(tabGroup);
+                            }
+                            return tabGroup; // the now existing tab group to append tab items
+                        }
                     }
-                    return tabGroup;  // the now existing tab group to append tab items
                 }
-            }
-        }
-    })
+            })
+        })
 }
 // end sidebar check
